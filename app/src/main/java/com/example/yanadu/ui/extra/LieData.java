c3aa4b52731b0a9923d3b90edb803165178f188b
shinhyunji <geeholic3@naver.com>
2021-11-25 04:49:33 +0900

    main&lie

---
+++ app/src/main/java/com/example/yanadu/ui/extra/LieData.java
@@ -0,0 +1,21 @@
+package com.example.yanadu.ui.extra;
+
+public class LieData {
+
+    private String et_mission;
+
+    public LieData(String et_mission) {
+        this.et_mission = et_mission;
+    }
+
+
+
+
+    public String getEt_mission() {
+        return et_mission;
+    }
+
+    public void setEt_mission(String et_mission) {
+        this.et_mission = et_mission;
+    }
+}
---
+++ app/src/main/java/com/example/yanadu/ui/extra/Lieadaptor.java
@@ -0,0 +1,83 @@
+package com.example.yanadu.ui.extra;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.yanadu.R;
+
+import java.util.ArrayList;
+
+public class Lieadaptor extends RecyclerView.Adapter<Lieadaptor.CustomViewHolder> {
+
+    private ArrayList<maindata>arrayList;
+
+    public Lieadaptor(ArrayList<maindata> arrayList) {
+        this.arrayList = arrayList;
+    }
+
+    @NonNull
+    @Override
+    public Lieadaptor.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent,false);
+        CustomViewHolder holder=new CustomViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Lieadaptor.CustomViewHolder holder, int position) {
+        holder.et_mission.setText(arrayList.get(position).getEt_mission());
+
+
+        holder.itemView.setTag(position);
+        holder.itemView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String curName=holder.et_mission.getText().toString();
+                Toast.makeText(view.getContext(),curName,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.itemView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                remove(holder.getAdapterPosition());
+                return;
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null!=arrayList?arrayList.size():0);
+    }
+
+    public void remove(int position){
+        try{
+            arrayList.remove(position);
+            notifyItemRemoved(position);
+
+        }catch (IndexOutOfBoundsException ex)
+        {
+            ex.printStackTrace();
+        }
+
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        protected EditText et_mission;
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.et_mission=(EditText) itemView.findViewById(R.id.et_mission);
+        }
+    }
+}
